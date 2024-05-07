13356ecd1a176833046584ad3f4be3832f72ad28
Douglascursos <158012009+Douglascursos@users.noreply.github.com>
2024-05-06 22:15:09 -0600

    Mi primer intento

---
+++ EjercicioAbstraccion/Program.cs
@@ -0,0 +1,19 @@
+﻿using EjercicioAbstraccion;
+
+string? n1, n2;
+
+Resultado operacion1 = new Resultado();
+
+Console.WriteLine("Ingresa el primer numero:");
+ n1 = Console.ReadLine();
+Console.WriteLine("Ingresa el segundo numero:");
+n2 = Console.ReadLine();
+
+operacion1.N1 = double.Parse(n1);
+operacion1.N2 = double.Parse(n2);
+
+operacion1.Imprimir();
+
+
+
+
---
+++ EjercicioAbstraccion/Resultado.cs
@@ -0,0 +1,46 @@
+﻿
+///Clase Abstracta 
+///Se crea
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioAbstraccion
+{
+    public class Resultado : Operaciones
+    {  
+        public double N1 { get; set; }
+        public double N2 { get; set; }
+
+        public override double Division()
+        {
+            return N1 / N2;
+        }
+
+        public override double Multiplicacion()
+        {
+            return N1 * N1;
+        }
+
+        public override double Resta()
+        {
+            return N1 - N1;
+        }
+
+        public override double Suma()
+        {
+            return N1 + N1;
+        }
+
+
+        public override void Imprimir()
+        {
+            Console.WriteLine($"Suma: {Suma()}");
+            Console.WriteLine($"Resta: {Resta()}");
+            Console.WriteLine($"Multiplicacion: {Multiplicacion()}");
+            Console.WriteLine($"Division: {Division()}");
+        }
+    }
+}
